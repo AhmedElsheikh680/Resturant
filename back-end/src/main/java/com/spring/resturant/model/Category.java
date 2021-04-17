84954742e799db5287df997a8ba7619c43066d44
Ahmed Elsheikh <ahmedelsheikh680@gmail.com>
2021-04-17 03:17:05 +0200

    Create Model(Category, Order) Closes #4

---
+++ back-end/src/main/java/com/spring/resturant/model/Category.java
@@ -0,0 +1,8 @@
+package com.spring.resturant.model;
+
+public class Category {
+
+    private Long id;
+
+    private String name;
+}
---
+++ back-end/src/main/java/com/spring/resturant/model/Order.java
@@ -0,0 +1,14 @@
+package com.spring.resturant.model;
+
+public class Order {
+
+    private Long id;
+
+    private String name;
+
+    private int price;
+
+    private String img;
+
+    private String description;
+}

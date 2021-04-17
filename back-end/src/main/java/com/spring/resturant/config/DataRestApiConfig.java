923604e6eb28c26f3b434fbcdb1d60d7feda272c
Ahmed Elsheikh <ahmedelsheikh680@gmail.com>
2021-04-17 15:48:03 +0200

    Prevent API of Jpa Repo Closes #15

---
+++ back-end/src/main/java/com/spring/resturant/config/DataRestApiConfig.java
@@ -0,0 +1,36 @@
+package com.spring.resturant.config;
+
+import com.spring.resturant.model.Category;
+import com.spring.resturant.model.Order;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+
+@Configuration
+public class DataRestApiConfig implements RepositoryRestConfigurer {
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+        HttpMethod[] preventMethod = {HttpMethod.GET, HttpMethod.POST, HttpMethod.PUT, HttpMethod.DELETE};
+        // Disable Http Methods For Category, Order : POST, PUT, DELETE
+        disableHttpMethods(Category.class, config, preventMethod);
+        disableHttpMethods(Order.class, config, preventMethod);
+
+
+//        config.getExposureConfiguration()
+//                .forDomainType(Category.class)
+//                .withItemExposure(((metdata, httpMethods) -> httpMethods.disable(preventMethod)))
+//                .withCollectionExposure(((metdata, httpMethods) -> httpMethods.disable(preventMethod)));
+//        config.getExposureConfiguration()
+//                .forDomainType(Order.class)
+//                .withItemExposure(((metdata, httpMethods) -> httpMethods.disable(preventMethod)))
+//                .withCollectionExposure(((metdata, httpMethods) -> httpMethods.disable(preventMethod)));
+
+    }
+    public void disableHttpMethods(Class theClass, RepositoryRestConfiguration config, HttpMethod[] preventMethod) {
+        config.getExposureConfiguration()
+                .forDomainType(theClass)
+                .withItemExposure(((metdata, httpMethods) -> httpMethods.disable(preventMethod)))
+                .withCollectionExposure(((metdata, httpMethods) -> httpMethods.disable(preventMethod)));
+    }
+    }

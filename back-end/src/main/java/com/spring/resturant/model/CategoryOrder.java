f7789e7d98cf8cd50dc22269a6cd74e5fc6724d8
Ahmed Elsheikh <ahmedelsheikh680@gmail.com>
2021-04-17 14:12:51 +0200

    @CreationTimestamp & @UpdateTimestamp Closes #11

--- back-end/src/main/java/com/spring/resturant/model/CategoryOrder.java
+++ back-end/src/main/java/com/spring/resturant/model/CategoryOrder.java
@@ -3,9 +3,12 @@ package com.spring.resturant.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import javax.persistence.Column;
 import javax.persistence.MappedSuperclass;
+import java.util.Date;
 
 @Data
 @NoArgsConstructor
@@ -15,5 +18,13 @@ public class CategoryOrder extends  BaseEntity{
 
     @Column(name = "name")
     private String name;
+
+    @CreationTimestamp
+    @Column(name = "data_created")
+    private Date dataCreated;
+
+    @UpdateTimestamp
+    @Column(name = "data_updated")
+    private Date dataUpdated;
 
 }

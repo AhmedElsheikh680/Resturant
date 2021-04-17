7b1102f440607d800aab60091f7625d6747caf50
Ahmed Elsheikh <ahmedelsheikh680@gmail.com>
2021-04-17 15:54:21 +0200

    create controller of category & order Closes  #17

---
+++ back-end/src/main/java/com/spring/resturant/controller/OrderController.java
@@ -0,0 +1,14 @@
+package com.spring.resturant.controller;
+
+import com.spring.resturant.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+@RequiredArgsConstructor
+public class OrderController {
+
+    private final OrderService orderService;
+}

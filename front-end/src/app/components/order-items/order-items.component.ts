588d315139a939a09916a1350f91780408d27f3a
Ahmed Elsheikh <ahmedelsheikh680@gmail.com>
2021-04-19 15:59:30 +0200

    show ordersByCategoryId in component Closes #32

--- front-end/src/app/components/order-items/order-items.component.ts
+++ front-end/src/app/components/order-items/order-items.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {OrderServiceService} from '../../services/order-service.service';
 import {Order} from '../../model/order';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-order-items',
@@ -10,13 +11,31 @@ import {Order} from '../../model/order';
 export class OrderItemsComponent implements OnInit {
 
   orders: Order[]  = [];
-  constructor(private orderService: OrderServiceService) { }
+  constructor(private orderService: OrderServiceService,
+              private route: ActivatedRoute) { }
 
   ngOnInit(): void {
-    this.getOrders()
+   this.finishOrders();
+  }
+  finishOrders(){
+    let result = this.route.snapshot.paramMap.has('id');
+    if(result){
+      this.getOrdersByCategoryId();
+    }else {
+      this.getOrders()
+
+    }
   }
   getOrders(){
     this.orderService.getOrders().subscribe(
+      data => {
+        this.orders = data
+      }
+    )
+  }
+  getOrdersByCategoryId(){
+    let categoryId = this.route.snapshot.paramMap.get('id');
+    this.orderService.getOrdersByCategoryId(categoryId).subscribe(
       data => {
         this.orders = data
       }

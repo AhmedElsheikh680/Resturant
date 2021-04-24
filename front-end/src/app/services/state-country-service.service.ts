c97c39108167144b5b44a8ad9b20f0adff68b88e
Ahmed Elsheikh <ahmedelsheikh680@gmail.com>
2021-04-24 12:50:34 +0200

    angular create model(country, state) and service Closes #57

---
+++ front-end/src/app/services/state-country-service.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {Country} from '../model/country';
+import {map} from 'rxjs/operators';
+import {State} from '../model/state';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StateCountryServiceService {
+
+  private baseUrl=`http://localhost:8080/api/v1`;
+
+  constructor(private httpClient: HttpClient) { }
+
+  getAllCountries(): Observable<Country[]>{
+    return this.httpClient.get<Country[]>(this.baseUrl+`/countries`).pipe(
+      map(
+        response => response
+      )
+    )
+  }
+
+  getAllStates(): Observable<State[]>{
+    return this.httpClient.get<State[]>(this.baseUrl+`/states`).pipe(
+      map(
+        response => response
+      )
+    )
+  }
+}

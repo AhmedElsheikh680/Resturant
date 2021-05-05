2883ce3e00dd333a26c2b4f4ab2eaa04d205ddd5
Ahmed Elsheikh <ahmedelsheikh680@gmail.com>
2021-05-05 15:26:08 +0200

    sign up parentFormGroup Closes  #91

--- front-end/src/app/components/signup/signup.component.ts
+++ front-end/src/app/components/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+
+  parentFromGroup: FormGroup;
+
+  constructor(private childFormGroup: FormBuilder) { }
 
   ngOnInit(): void {
+    this.myFormSignup()
+  }
+  myFormSignup(){
+    this.parentFromGroup = this.childFormGroup.group({
+      user: this.childFormGroup.group({
+        email: [''],
+        password: ['']
+      })
+    })
   }
 
+  signup() {
+    alert(this.parentFromGroup.controls['user'].value.email);
+    alert(this.parentFromGroup.controls['user'].value.password);
+  }
 }
